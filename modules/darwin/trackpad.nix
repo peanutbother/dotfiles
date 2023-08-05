aa40236f77de4a06fbd831c66a752108755b737a
peanutbother <peanutbother@proton.me>
2023-08-06 00:35:18 +0200

    formatting + documentation

--- modules/darwin/trackpad.nix
+++ modules/darwin/trackpad.nix
@@ -1,8 +1,14 @@
 {
-  system.defaults.trackpad = {
-    Clicking = true;
-    ActuationStrength = 0;
-    FirstClickThreshold = 1;
-    SecondClickThreshold = 1;
+  system.defaults = {
+    trackpad = {
+      Clicking = true;
+      ActuationStrength = 0; # force feedback
+      FirstClickThreshold = 1; # force feedback
+      SecondClickThreshold = 1; # force feedback
+    };
+    NSGlobalDomain = {
+      "com.apple.trackpad.scaling" = 2.0; # tracking speed
+      "com.apple.mouse.tapBehavior" = 1; # tap to click
+    };
   };
 }
