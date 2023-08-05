2c4af7a4aa28f4d419affa4d7d6950c3929ccf36
peanutbother <peanutbother@proton.me>
2023-08-06 00:31:53 +0200

    configure dock

--- modules/darwin/dock.nix
+++ modules/darwin/dock.nix
@@ -1,5 +1,8 @@
 {
   system.defaults.dock = {
     autohide = true;
+    magnification = false; # don't magnify on hover
+    tilesize = 42; # dock icon size
+    wvous-tl-corner = 11; # launchpad as hot corner top left
   };
 }
