b26b56a48d69203706b086a993dbddaf963f2cee
peanutbother <peanutbother@proton.me>
2023-08-06 17:40:17 +0200

    restyle continuation prompt

--- modules/home-manager/programs/starship.nix
+++ modules/home-manager/programs/starship.nix
@@ -6,7 +6,7 @@
     settings = {
       format = "[](#9A348E)$os$username[](bg:#DA627D fg:#9A348E)$directory[](fg:#DA627D bg:#FCA17D)$git_branch$git_status[](fg:#FCA17D bg:#86BBD8)$c$elixir$elm$golang$gradle$haskell$java$julia$nodejs$nim$rust$scala[](fg:#86BBD8 bg:#06969A)$docker_context[](fg:#06969A bg:#33658A)$custom[ ](fg:#33658A)";
 
-      continuation_prompt = "[](fg:#9A348E)[…](bg:#9A348E)[█](fg:#9A348E) ";
+      continuation_prompt = "󱞩 ";
 
       custom.direnv = {
         format = "[  ](bg:#33658A)";
