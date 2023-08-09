eb29ab9ac109d4c00388213058c6451c567c668f
peanutbother <peanutbother@proton.me>
2023-08-09 07:38:16 +0200

    add age secret key to bootstrap process

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,10 @@
 echo "installing nix"
 eval "$(curl -L https://nixos.org/nix/install)"
 
+echo "copying keys"
+mkdir -p /Users/yuna/Library/Application\ Support/sops/age/
+cp secres/keys.txt /Users/yuna/Library/Application\ Support/sops/age/keys.txt
+
 echo "enabling flakes"
 mkdir -p ~/.config/nix
 echo "experimental-features = nix-command flakes" >>~/.config/nix/nix.conf
