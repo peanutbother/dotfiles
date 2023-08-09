45d802c5f6c2433b873396c8c67a80b6ba1c86ad
peanutbother <peanutbother@proton.me>
2023-08-09 07:37:57 +0200

    add new ssh key

--- modules/home-manager/dotfiles/default.nix
+++ modules/home-manager/dotfiles/default.nix
@@ -2,6 +2,7 @@
   home.file = {
     ".cargo/config".source = ./cargo/config;
     ".config/tmux/tmux-nerd-font-window-name.yml".source = ./tmux/tmux-nerd-font-window-name.yml;
+    ".ssh/dev_bricksoft.pub".source = ./ssh/dev_bricksoft.pub;
     ".ssh/dev_ravpower.pub".source = ./ssh/dev_ravpower.pub;
     ".ssh/github_peanutbother.pub".source = ./ssh/github_peanutbother.pub;
   };
