4a069e2ade5ba16523b2918312377a0d3df2618f
peanutbother <peanutbother@proton.me>
2023-08-09 07:37:33 +0200

    fix key names

--- modules/home-manager/programs/sops.nix
+++ modules/home-manager/programs/sops.nix
@@ -3,11 +3,14 @@
     defaultSopsFile = ../../../secrets/yuna.yaml;
     defaultSopsFormat = "yaml";
 
-    age.keyFile = "${home}/.config/sops/age/keys.txt";
+    age.keyFile = "${home}/Library/Application\ Support/sops/age/keys.txt";
 
     secrets = {
       "ssh-keys/git/peanutbother" = {
         path = "${home}/.ssh/github_peanutbother";
+      };
+      "ssh-keys/devs/bricksoft" = {
+        path = "${home}/.ssh/dev_bricksoft";
       };
       "ssh-keys/devs/ravpower" = {
         path = "${home}/.ssh/dev_ravpower";
