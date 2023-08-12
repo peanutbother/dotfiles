5b95ca966ff1397fd356324f2e0b8cfc1f19e0cc
peanutbother <peanutbother@proton.me>
2023-08-12 20:51:19 +0200

    add nix template methods for zsh

--- modules/home-manager/programs/zsh.nix
+++ modules/home-manager/programs/zsh.nix
@@ -7,9 +7,15 @@
 
     autocd = true;
     envExtra = ''
-      # initialize nix development shells with this alias
+      # initialize nix development shells with these aliases
       develop(){
         nix develop ~/.nix#$1 --show-trace
+      }
+      init_develop(){
+        nix flake init --template $1 --show-trace && direnv allow
+      }
+      create_develop(){
+        nix flake new --template $1 $2 --show-trace && direnv allow
       }
     '';
   };
