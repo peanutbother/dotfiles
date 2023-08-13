263ab099fa4114a17363054f9e3fd3c5631d7e25
peanutbother <peanutbother@proton.me>
2023-08-13 18:20:56 +0200

    add  git url shortcuts

--- modules/home-manager/programs/git.nix
+++ modules/home-manager/programs/git.nix
@@ -9,8 +9,13 @@
       pull.rebase = false;
       push.autoSetupRemote = true;
 
+      # url rewrites to ssh
       url."ssh://git@github.com".pushInsteadOf = "https://github.com";
       url."ssh://git@gitlab.com".pushInsteadOf = "https://gitlab.com";
+
+      # url aliases
+      url."https://github.com/".insteadOf = "gh";
+      url."https://gitlab.com/".insteadOf = "gl";
     };
   };
 }
