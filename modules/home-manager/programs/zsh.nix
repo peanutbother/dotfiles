afab81b548d061cc4c70ef698423975cb842c50a
peanutbother <peanutbother@proton.me>
2023-08-13 17:55:09 +0200

    rename develop aliases

--- modules/home-manager/programs/zsh.nix
+++ modules/home-manager/programs/zsh.nix
@@ -11,10 +11,10 @@
       develop(){
         nix develop ~/.nix#$1 --show-trace
       }
-      init_develop(){
+      develop_init(){
         nix flake init --template $1 --show-trace && direnv allow
       }
-      create_develop(){
+      develop_create(){
         nix flake new --template $1 $2 --show-trace && direnv allow
       }
 
