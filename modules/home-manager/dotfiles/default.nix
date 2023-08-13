475c51c002d9a770127ac2f54bd7137684c88662
peanutbother <peanutbother@proton.me>
2023-08-13 17:03:40 +0200

    fix missing dotfile config

--- modules/home-manager/dotfiles/default.nix
+++ modules/home-manager/dotfiles/default.nix
@@ -8,6 +8,10 @@
       source = ./tmux;
       recursive = true;
     };
+    ".config/tmuxinator" = {
+      source = ./tmuxinator;
+      recursive = true;
+    };
     ".ssh" = {
       source = ./ssh;
       recursive = true;
