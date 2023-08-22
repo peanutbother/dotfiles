e521f5d69b03e03fbb5a70de3361f5584bc3a319
peanutbother <peanutbother@proton.me>
2023-08-22 23:21:22 +0200

    remove wrong user config

--- modules/darwin/networking.nix
+++ modules/darwin/networking.nix
@@ -2,10 +2,6 @@
   system.defaults.alf.globalstate = 1; # enable firewall
 
   networking = {
-    computerName = "yubook";
-    hostName = "yubook";
-    localHostName = "yubook";
-
     # AdGuard DNS
     dns = [
       "94.140.14.14"
