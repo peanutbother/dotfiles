64d076f81649c2def2cd4eaa1efc7ff38ae9055f
peanutbother <peanutbother@proton.me>
2023-08-23 11:46:02 +0200

    make git config fancier

--- modules/home-manager/programs/git.nix
+++ modules/home-manager/programs/git.nix
@@ -1,21 +1,28 @@
 {
-  programs.git = {
-    enable = true;
-    userName = "peanutbother";
-    userEmail = "peanutbother@proton.me";
-    extraConfig = {
-      init.defaultBranch = "main";
+  programs.git =
+    let
+      gh = "github.com";
+      gl = "gitlab.com";
+      mkURLConfig = host: shortcut: {
+        # url rewrites to ssh
+        "ssh://git@${host}".pushInsteadOf = "https://${host}";
+        # url aliases
+        "https://${host}".insteadOf = shortcut;
+      };
+    in
+    {
+      enable = true;
+      userName = "peanutbother";
+      userEmail = "peanutbother@proton.me";
+      extraConfig = {
+        init.defaultBranch = "main";
 
-      pull.rebase = false;
-      push.autoSetupRemote = true;
+        pull.rebase = false;
+        push.autoSetupRemote = true;
 
-      # url rewrites to ssh
-      url."ssh://git@github.com".pushInsteadOf = "https://github.com";
-      url."ssh://git@gitlab.com".pushInsteadOf = "https://gitlab.com";
-
-      # url aliases
-      url."https://github.com/".insteadOf = "gh";
-      url."https://gitlab.com/".insteadOf = "gl";
+        url = { }
+          // mkURLConfig gh "gh"
+          // mkURLConfig gl "gl";
+      };
     };
-  };
 }
