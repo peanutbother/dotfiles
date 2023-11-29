54966d0b5f26dbecf97a306bfa4278c7ac54e7b6
peanutbother <peanutbother@proton.me>
2023-11-29 20:32:08 +0100

    remove unnecessary initrd options

--- hosts/yunix/system.nix
+++ hosts/yunix/system.nix
@@ -9,12 +9,6 @@
 
   boot = {
     initrd = {
-      # Setup keyfile
-      secrets = {
-        # "/crypto_keyfile.bin" = null;
-      };
-      # Setup luks disk
-      # luks.devices."luks-ccc08f94-9a1f-4c65-8498-a18da59d6471".device = "/dev/disk/by-uuid/ccc08f94-9a1f-4c65-8498-a18da59d6471";
       # Kernel modules in initrd
       availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod" "thunderbolt" ];
       kernelModules = [ "kvm-intel" "brcmfmac" ];
