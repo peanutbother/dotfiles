cdaad1188ae1fa5966e7240620088469d354056e
peanutbother <peanutbother@proton.me>
2023-12-04 15:09:56 +0100

    cleanup system config

--- hosts/yubook/system.nix
+++ hosts/yubook/system.nix
@@ -2,15 +2,10 @@
   system.stateVersion = 4;
 
   homebrew = {
-    # caskArgs.no_quarantine = false;
     masApps = {
       WCHSerialPort = 1580700717;
     };
-    # taps = [
-    #   "armcord/armcord"
-    # ];
     casks = [
-      # "armcord"
       "discord"
       "istat-menus"
       "mounty"
