80520c34321e30bd15e37c3ad636e3b2b05d9fa6
peanutbother <peanutbother@proton.me>
2023-12-04 12:50:49 +0100

    manage vsocde with exts

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,4 +4,9 @@
   (import ./direnv.nix)
   (import ./plymouth)
   (import ./sddm)
+  (import (builtins.fetchGit {
+    url = "https://github.com/nix-community/nix-vscode-extensions";
+    ref = "refs/heads/master";
+    rev = "a72a237b9929ce7d8163617747802553e6c7fdc0";
+  })).overlays.default
 ]
