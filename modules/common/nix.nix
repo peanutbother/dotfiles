da119d12b4e83f28bcaf35631e4d30c1b4a3c050
peanutbother <peanutbother@proton.me>
2023-12-04 12:51:30 +0100

    add nix-community cachix instance

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -9,6 +9,13 @@
       auto-optimise-store = lib.mkDefault true;
       # sandbox builds
       sandbox = lib.mkDefault true;
+      # enable binary caches
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     # enable garbage collection
     gc = {
