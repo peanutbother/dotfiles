ac893640307048975a6085cdc214b8a2777bd43b
peanutbother <peanutbother@proton.me>
2024-05-15 21:33:34 +0200

    add nix-homebrew to yubook

--- hosts/yubook/default.nix
+++ hosts/yubook/default.nix
@@ -8,6 +8,25 @@ in
   # system
   (mkSystem { darwin = true; })
 
+
+  # homebrew
+  nix-homebrew.darwinModules.nix-homebrew
+  {
+    nix-homebrew = {
+      inherit user;
+
+      enable = true;
+      enableRosetta = true;
+      mutableTaps = true;
+      autoMigrate = true;
+
+      taps = {
+        "homebrew/homebrew-core" = homebrew-core;
+        "homebrew/homebrew-cask" = homebrew-cask;
+      };
+    };
+  }
+
   # home
   home-manager.darwinModule
   (mkHome { inherit user repo; })
