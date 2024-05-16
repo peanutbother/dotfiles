3f97c42cd9f9e14a7a07be7255beaff35b59ae43
peanutbother <peanutbother@proton.me>
2024-05-16 19:46:33 +0200

    fix: remove redundant imports from quirks

--- hosts/yunix/default.nix
+++ hosts/yunix/default.nix
@@ -7,9 +7,7 @@ in
 [
   # quirks
   nixos-hardware.nixosModules.apple-macbook-pro-12-1
-  nixos-hardware.nixosModules.common-cpu-intel
   nixos-hardware.nixosModules.common-hidpi
-  nixos-hardware.nixosModules.common-pc-ssd
 
   # system
   (mkSystem { })
