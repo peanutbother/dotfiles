e15fe653f5afdd1f473a49d8f4d6a2cb3ea58073
peanutbother <peanutbother@proton.me>
2024-05-16 21:38:42 +0200

    refactor to options for git shortcuts and tmux urlview plugin

--- modules/home-manager/programs/git.nix
+++ modules/home-manager/programs/git.nix
@@ -1,26 +1,43 @@
-{ lib, ... }: {
-  programs.git =
-    let
-      gh = "github.com";
-      gl = "gitlab.com";
-      mkURLConfig = host: shortcut: {
-        # url rewrites to ssh
-        "ssh://git@${host}".pushInsteadOf = "https://${host}";
-        # url aliases
-        "https://${host}".insteadOf = shortcut;
+{ lib, config, ... }:
+with lib; let
+  mkURLConfig = host: shortcut: {
+    # url rewrites to ssh
+    "ssh://git@${host}".pushInsteadOf = "https://${host}";
+    # url aliases
+    "https://${host}".insteadOf = shortcut;
+  };
+  cfg = config.programs.git;
+in
+{
+  options = {
+    programs.git.shortcuts = mkOption {
+      type = with types; attrsOf str;
+      description = "creates url aliases for git sources";
+      default = {
+        "gh" = "github.com";
+        "gl" = "gitlab.com";
       };
-    in
-    {
-      enable = lib.mkDefault true;
-      extraConfig = {
-        init.defaultBranch = lib.mkDefault "main";
-
-        pull.rebase = lib.mkDefault false;
-        push.autoSetupRemote = lib.mkDefault true;
-
-        url = { }
-          // mkURLConfig gh "gh"
-          // mkURLConfig gl "gl";
+      example = {
+        "gh" = "github.com";
+        "gl" = "gitlab.com";
       };
     };
+  };
+
+  config = {
+    programs.git =
+      {
+        enable = lib.mkDefault true;
+        extraConfig = {
+          init.defaultBranch = lib.mkDefault "main";
+
+          pull.rebase = lib.mkDefault false;
+          push.autoSetupRemote = lib.mkDefault true;
+
+          url = (foldl (a: b: a // b) { } (
+            mapAttrsToList (shortcut: host: mkURLConfig host shortcut) cfg.shortcuts
+          ));
+        };
+      };
+  };
 }
--- modules/home-manager/programs/tmux/default.nix
+++ modules/home-manager/programs/tmux/default.nix
@@ -1,53 +1,63 @@
-{ lib, pkgs, ... }:
-let
+{ lib, pkgs, config, ... }:
+with lib; let
   plugins = pkgs.callPackage ./plugins.nix { };
+  cfg = config.programs.tmux;
+  plgs = with pkgs.tmuxPlugins; with plugins; [
+    # theme
+    {
+      plugin = catppuccin;
+      extraConfig = ''
+        set -g @catppuccin_flavour 'macchiato'
+        set -g @catppuccin_pill_theme_enabled on
+        set -g @catppuccin_window_tabs_enabled on
+        set -g @catppuccin_host "on"
+      '';
+    }
+    # shell
+    tmux-nerd-font-window-name-show-name
+
+    # control
+    better-mouse-mode
+    pain-control
+
+    # utilities
+    tmux-fzf
+    tmux-notify
+    logging
+  ];
 in
 {
-  programs.tmux = {
-    enable = lib.mkDefault true;
-    clock24 = true;
-    mouse = true;
-    shell = "${pkgs.zsh}/bin/zsh";
-    shortcut = lib.mkDefault "y";
-    terminal = "xterm-256color";
-    plugins = with pkgs.tmuxPlugins; with plugins; [
-      # theme
-      {
-        plugin = catppuccin;
-        extraConfig = ''
-          set -g @catppuccin_flavour 'macchiato'
-          set -g @catppuccin_pill_theme_enabled on
-          set -g @catppuccin_window_tabs_enabled on
-          set -g @catppuccin_host "on"
-        '';
-      }
-      # shell
-      tmux-nerd-font-window-name-show-name
+  options = {
+    programs.tmux.urlview = mkOption {
+      type = types.bool;
+      description = "whether to enable urlview plugin";
+      default = false; # disabled by default currently because it does not compile
+    };
+  };
+  config = {
+    programs.tmux = {
+      enable = mkDefault true;
+      clock24 = true;
+      mouse = true;
+      shell = "${pkgs.zsh}/bin/zsh";
+      shortcut = mkDefault "y";
+      terminal = "xterm-256color";
+      plugins = mkDefault (if cfg.urlview then plgs ++ [ pkgs.tmuxPlugins.urlview ] else plgs);
+      extraConfig = ''
+        # Hack Terminfo to force 24 bit color support flags for my favourite terminal
+        set-option -sa terminal-overrides ",xterm-*:Tc"
+        set-option -sa terminal-overrides ",tmux-*:RGB"
+        set-option -sa terminal-overrides ",alacritty:RGB"
 
-      # control
-      better-mouse-mode
-      pain-control
-      urlview
+        # set status bar to top
+        set-option -g status-position top
 
-      # utilities
-      tmux-fzf
-      tmux-notify
-      logging
-    ];
-    extraConfig = ''
-      # Hack Terminfo to force 24 bit color support flags for my favourite terminal
-      set-option -sa terminal-overrides ",xterm-*:Tc"
-      set-option -sa terminal-overrides ",tmux-*:RGB"
-      set-option -sa terminal-overrides ",alacritty:RGB"
+        bind S run-shell "tmux popup -y 10 -w 100 -h 50 -E ${pkgs.cht-sh}/tmux.sh"
 
-      # set status bar to top
-      set-option -g status-position top
-
-      bind S run-shell "tmux popup -y 10 -w 100 -h 50 -E ${pkgs.cht-sh}/tmux.sh"
-
-      # sending panes back and forth (https://unix.stackexchange.com/a/14301/220900)
-      bind-key < command-prompt -p "join pane from:"  "join-pane -s '%%'"
-      bind-key > command-prompt -p "send pane to:"  "join-pane -t '%%'"
-    '';
+        # sending panes back and forth (https://unix.stackexchange.com/a/14301/220900)
+        bind-key < command-prompt -p "join pane from:"  "join-pane -s '%%'"
+        bind-key > command-prompt -p "send pane to:"  "join-pane -t '%%'"
+      '';
+    };
   };
 }
