d2d66edacfd873493c5a605b46681fe78bd206d9
peanutbother <peanutbother@proton.me>
2024-05-23 19:45:33 +0200

    fix util invocation from shell

--- shells/embedded/flake.nix
+++ shells/embedded/flake.nix
@@ -3,29 +3,33 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
-  outputs = { nixpkgs, ... }: (import ../../util.nix).eachSystem
-    (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        packages = with pkgs; [
-          avrdude
-          rustup
-        ];
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          inherit packages;
-          shellHook = ''
-            if [ -f .env ] && [ -z "$__DIRENV_SOURCED" ]; then
-              echo direnv: loading local .env
-              export __DIRENV_SOURCED=1
-              source .env
-            fi
+  outputs = { nixpkgs, ... } @inputs:
+    let
+      stateVersion = "23.11";
+    in
+    (import ../../util.nix inputs stateVersion).eachSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          packages = with pkgs; [
+            avrdude
+            rustup
+          ];
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            inherit packages;
+            shellHook = ''
+              if [ -f .env ] && [ -z "$__DIRENV_SOURCED" ]; then
+                echo direnv: loading local .env
+                export __DIRENV_SOURCED=1
+                source .env
+              fi
             
-            avrdude 2>&1 1>/dev/null | tail -n 1
-            rustc --version
-          '';
-        };
-      }
-    );
+              avrdude 2>&1 1>/dev/null | tail -n 1
+              rustc --version
+            '';
+          };
+        }
+      );
 }
--- shells/rust/flake.nix
+++ shells/rust/flake.nix
@@ -3,31 +3,35 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
-  outputs = { nixpkgs, ... }: (import ../../util.nix).eachSystem
-    (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        packages = with pkgs; [
-          iconv
-          rustup
-        ] ++ (if system == "aarch64-darwin" || system == "x86_64-darwin" then with pkgs; [
-          darwin.apple_sdk.frameworks.Security
-          darwin.apple_sdk.frameworks.SystemConfiguration
-        ] else [ ]);
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          inherit packages;
-          shellHook = ''
-            if [ -f .env ] && [ -z "$__DIRENV_SOURCED" ]; then
-              echo direnv: loading local .env
-              export __DIRENV_SOURCED=1
-              source .env
-            fi
+  outputs = { nixpkgs, ... } @inputs:
+    let
+      stateVersion = "23.11";
+    in
+    (import ../../util.nix inputs stateVersion).eachSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          packages = with pkgs; [
+            iconv
+            rustup
+          ] ++ (if system == "aarch64-darwin" || system == "x86_64-darwin" then with pkgs; [
+            darwin.apple_sdk.frameworks.Security
+            darwin.apple_sdk.frameworks.SystemConfiguration
+          ] else [ ]);
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            inherit packages;
+            shellHook = ''
+              if [ -f .env ] && [ -z "$__DIRENV_SOURCED" ]; then
+                echo direnv: loading local .env
+                export __DIRENV_SOURCED=1
+                source .env
+              fi
             
-            rustc --version
-          '';
-        };
-      }
-    );
+              rustc --version
+            '';
+          };
+        }
+      );
 }
