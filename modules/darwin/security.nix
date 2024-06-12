b3767b254e66423b5314980e820ca1a1072a71ef
peanutbother <peanutbother@proton.me>
2024-06-12 14:48:58 +0200

    add pam_reattach, create option, make defaults for touchid sudo

--- modules/darwin/security.nix
+++ modules/darwin/security.nix
@@ -1,3 +1,16 @@
-{ lib, ... }: {
-  security.pam.enableSudoTouchIdAuth = lib.mkDefault true;
+{ lib, config, pkgs, ... }:
+let cfg = config.security; in
+{
+  options = {
+    security.useTouchId = lib.mkOption {
+      type = lib.types.bool;
+      description = "whether to enable touchid in sudo prompts";
+      default = true;
+      example = false;
+    };
+  };
+  config = {
+    security.pam.enableSudoTouchIdAuth = lib.mkDefault cfg.useTouchId;
+    security.sudo.extraConfig = lib.mkDefault (if cfg.useTouchId then "auth       optional       ${pkgs.unstable.pam-reattach}/lib/pam/pam_reattach.so" else "");
+  };
 }
