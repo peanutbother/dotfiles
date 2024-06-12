464b167c7d7ab2a6d3dc0f420d2bf13563ec21aa
peanutbother <peanutbother@proton.me>
2024-06-12 14:52:17 +0200

    fix negative float syntax in acceleration default

--- modules/darwin/mouse.nix
+++ modules/darwin/mouse.nix
@@ -1,3 +1,3 @@
 { lib, ... }: {
-  system.defaults.".GlobalPreferences"."com.apple.mouse.scaling" = lib.mkDefault - 1.0; # disable mouse acceleration
+  system.defaults.".GlobalPreferences"."com.apple.mouse.scaling" = lib.mkDefault (-1.0); # disable mouse acceleration
 }
