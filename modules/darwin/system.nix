389b84fcc5306f5ee259d884ab2788ca62a39ad6
peanutbother <peanutbother@proton.me>
2024-06-14 21:10:21 +0200

    disable startup chime, restructure option namespace

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -1,18 +1,21 @@
 { lib, ... }: {
-  system.defaults = {
-    loginwindow = {
-      DisableConsoleAccess = lib.mkDefault true;
-      GuestEnabled = lib.mkDefault false;
-      SHOWFULLNAME = lib.mkDefault false;
-    };
-    menuExtraClock = {
-      Show24Hour = lib.mkDefault true;
-      ShowDate = lib.mkDefault 0;
-      ShowDayOfMonth = lib.mkDefault true;
-    };
-    NSGlobalDomain = {
-      "com.apple.sound.beep.feedback" = lib.mkDefault 0; # disable sound when changing volume
-      AppleInterfaceStyleSwitchesAutomatically = lib.mkDefault true; # auto dark / light mode
+  system = {
+    startup.chime = lib.mkDefault false;
+    defaults = {
+      loginwindow = {
+        DisableConsoleAccess = lib.mkDefault true;
+        GuestEnabled = lib.mkDefault false;
+        SHOWFULLNAME = lib.mkDefault false;
+      };
+      menuExtraClock = {
+        Show24Hour = lib.mkDefault true;
+        ShowDate = lib.mkDefault 0;
+        ShowDayOfMonth = lib.mkDefault true;
+      };
+      NSGlobalDomain = {
+        "com.apple.sound.beep.feedback" = lib.mkDefault 0; # disable sound when changing volume
+        AppleInterfaceStyleSwitchesAutomatically = lib.mkDefault true; # auto dark / light mode
+      };
     };
   };
 }
