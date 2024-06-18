af1e9ef5d3b75be66f1e5e8f2029a762577a82cc
peanutbother <peanutbother@proton.me>
2024-06-18 14:08:09 +0200

    enable mac-app-util for darwin targets

--- hosts/YunAir/default.nix
+++ hosts/YunAir/default.nix
@@ -26,6 +26,9 @@ in
     };
   }
 
+  # link nix apps to darwin (fix spotlight, dock)
+  mac-app-util.darwinModules.default
+
   # home
   home-manager.darwinModule
   (mkHome { inherit user repo; })
--- hosts/yubook/default.nix
+++ hosts/yubook/default.nix
@@ -27,6 +27,9 @@ in
     };
   }
 
+  # link nix apps to darwin (fix spotlight, dock)
+  mac-app-util.darwinModules.default
+
   # home
   home-manager.darwinModule
   (mkHome { inherit user repo; })
