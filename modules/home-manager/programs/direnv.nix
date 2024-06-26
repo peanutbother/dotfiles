ad74ec1a031ad8ecbb0e07257477ce394b4afbe8
peanutbother <peanutbother@proton.me>
2024-06-26 14:06:08 +0200

    use conditional options for direnv

--- modules/home-manager/programs/direnv.nix
+++ modules/home-manager/programs/direnv.nix
@@ -1,16 +1,16 @@
-{ lib, ... }: {
+{ lib, config, ... }: {
   programs.direnv = {
     enable = lib.mkDefault true;
     enableZshIntegration = lib.mkDefault true;
 
-    nix-direnv.enable = true;
+    nix-direnv.enable = lib.mkDefault config.programs.direnv.enable;
 
     config.global = {
       warn_timeout = "1m";
     };
   };
   # hide env variable diffs of direnv
-  programs.zsh.envExtra = ''
+  programs.zsh.envExtra = lib.mkIf config.programs.direnv.enable ''
     export DIRENV_HIDE_DIFF=1;
   '';
 }
