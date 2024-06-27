0893080578242684c053efceb8a9a4394729c6ec
peanutbother <peanutbother@proton.me>
2024-06-27 19:16:36 +0200

    fix repo path in zsh function

--- modules/home-manager/programs/zsh/nix.nix
+++ modules/home-manager/programs/zsh/nix.nix
@@ -1,9 +1,9 @@
-{config, ...}: {
+{repo, ...}: {
   programs.zsh = {
     envExtra = ''
       # initialize nix development shells with these aliases
       develop(){
-        nix develop ~/.nix#$1 --show-trace
+        nix develop ${repo}#$1 --show-trace
       }
       develop_init(){
         nix flake init --template $1 --show-trace && direnv allow
