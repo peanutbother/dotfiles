032f4a18536b49110145dd3a0bbf6fcd8c216c27
peanutbother <peanutbother@proton.me>
2024-06-27 19:18:38 +0200

    fix extended glob match

--- modules/home-manager/programs/zsh/nix.nix
+++ modules/home-manager/programs/zsh/nix.nix
@@ -3,13 +3,13 @@
     envExtra = ''
       # initialize nix development shells with these aliases
       develop(){
-        nix develop ${repo}#$1 --show-trace
+        nix develop "${repo}#$1" --show-trace
       }
       develop_init(){
-        nix flake init --template $1 --show-trace && direnv allow
+        nix flake init --template "$1" --show-trace && direnv allow
       }
       develop_create(){
-        nix flake new --template $1 $2 --show-trace && direnv allow
+        nix flake new --template "$1" $2 --show-trace && direnv allow
       }
     '';
   };
