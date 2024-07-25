f9e581d35962b4e9cbdb5086a9f583185b0b8bf0
peanutbother <peanutbother@proton.me>
2024-07-25 09:12:49 +0200

    nix: add trusted users default

--- modules/common/nix/default.nix
+++ modules/common/nix/default.nix
@@ -20,6 +20,10 @@
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
+      trusted-users = lib.mkDefault [
+        "root"
+        "@wheel"
+      ];
     };
     # enable garbage collection
     gc = {
