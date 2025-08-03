49d28df8dedeaf6032691d7642d17947c81d6158
peanutbother <peanutbother@proton.me>
2025-08-03 15:38:23 +0200

    update rectangle

--- overlays/rectangle.nix
+++ overlays/rectangle.nix
@@ -1,9 +1,9 @@
 final: prev: {
   rectangle = prev.rectangle.overrideAttrs (_: prev: rec {
-    version = "0.88";
+    version = "0.89";
     src = builtins.fetchurl {
       url = "https://github.com/rxhanson/Rectangle/releases/download/v${version}/Rectangle${version}.dmg";
-      sha256 = "sha256:0ixmx088p1kqixl3skdiszvwd67mmk9nwwby16s0z67yr6xyfav3";
+      sha256 = "sha256:0k8dbg3d5hxd9264pi42l5cg4rhsd3m2kl92djq151y9f3xc53bq";
     };
   });
 }
