7d00f0f198799d83111e4b20e8c6a3d6c9b59c64
peanutbother <peanutbother@proton.me>
2025-08-03 15:38:10 +0200

    update alt-tab

--- overlays/alt-tab-macos.nix
+++ overlays/alt-tab-macos.nix
@@ -1,9 +1,9 @@
 final: prev: {
   alt-tab-macos = prev.alt-tab-macos.overrideAttrs (_: prev: rec {
-    version = "7.25.0";
+    version = "7.26.0";
     src = builtins.fetchurl {
       url = "https://github.com/lwouis/alt-tab-macos/releases/download/v${version}/AltTab-${version}.zip";
-      sha256 = "sha256:0ji1vpq8n4xvp6n5m8rhq0657kws78sxf3yqwcifs76h8ygxwpbv";
+      sha256 = "sha256:16xhlvk11h8h46z0nvbxyrn2dxs6m2c3hkz9j7v11xkhaqcbwg5l";
     };
   });
 }
