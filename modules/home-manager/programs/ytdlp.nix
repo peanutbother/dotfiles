a2bae0f4107bb9d443f624350b50e82a7e5e2217
peanutbother <peanutbother@proton.me>
2025-08-18 21:49:05 +0200

    tidy up nesting

--- modules/home-manager/programs/ytdlp.nix
+++ modules/home-manager/programs/ytdlp.nix
@@ -7,16 +7,14 @@
     enable = lib.mkDefault true;
   };
 
-  programs.zsh = {
-    envExtra = lib.mkIf config.programs.yt-dlp.enable ''
-      # Download given URL as mp3 via youtube-dl into ~/Downloads
-      yta(){
-        yt-dlp -i -c -o "~/Downloads/%(title)s-%(id)s.%(ext)s" --extract-audio --audio-format mp3 --audio-quality 0 "$1"
-      }
-      # Download given URL as best single file video quality resolution via youtube-dl into ~/Downloads
-      ytv(){
-        yt-dlp "$1" -o "~/Downloads/%(title)s-%(id)s.%(ext)s"
-      }
-    '';
-  };
+  programs.zsh.envExtra = lib.mkIf config.programs.yt-dlp.enable ''
+    # Download given URL as mp3 via youtube-dl into ~/Downloads
+    yta(){
+      yt-dlp -i -c -o "~/Downloads/%(title)s-%(id)s.%(ext)s" --extract-audio --audio-format mp3 --audio-quality 0 "$1"
+    }
+    # Download given URL as best single file video quality resolution via youtube-dl into ~/Downloads
+    ytv(){
+      yt-dlp "$1" -o "~/Downloads/%(title)s-%(id)s.%(ext)s"
+    }
+  '';
 }
