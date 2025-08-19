195c727281e390393d5ad03be33a7d14e2b25207
peanutbother <peanutbother@proton.me>
2025-08-19 19:37:24 +0200

    tidy up nesting

--- modules/util/hosts.nix
+++ modules/util/hosts.nix
@@ -52,7 +52,7 @@ with inputs.nixpkgs; rec {
     homebrewOptions ? {},
     ...
   }:
-    inputs.nixpkgs.lib.nixosSystem {
+    lib.nixosSystem {
       inherit system;
       modules = mkModules {inherit user host modules;} false;
       specialArgs = mkArgs {
