da6e37f965d70d070fa4df3ffac9fe3840ca177d
peanutbother <peanutbother@proton.me>
2025-08-22 10:10:01 +0200

    restructure signal expression; update signal

--- overlays/signal.nix
+++ overlays/signal.nix
@@ -1,16 +1,17 @@
 final: prev: let
-  version = "7.66.0";
-  hash = ""; # TODO supply signal hash for linux
+  version = "7.67.0";
 in {
   signal-desktop-bin = prev.signal-desktop-bin.overrideAttrs (_: prev:
     if final.system != "aarch64-darwin" && final.system != "x86_64-darwin"
     then {
-      inherit hash version;
+      inherit version;
+      hash = ""; # TODO supply signal hash for linux
     }
     else {
+      inherit version;
       src = builtins.fetchurl {
         url = "https://updates.signal.org/desktop/signal-desktop-mac-universal-${version}.dmg";
-        sha256 = "sha256:03q9zd420y5lrn7si5jpi58w72jy7lb7xls14jgh1qh0h4j6gdgf";
+        sha256 = "sha256:1n8z30y7ryrqb7xqyapbbykf2zbmpz38bkj4llhdsh4lh0v6z6fp";
       };
     });
 }
