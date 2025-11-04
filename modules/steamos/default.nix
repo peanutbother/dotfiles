6b77fee4944e5a0468b780f12697148f4cd0ef55
peanutbother <peanutbother@proton.me>
2025-11-04 12:10:29 +0100

    fix steamos module order

--- modules/steamos/default.nix
+++ modules/steamos/default.nix
@@ -1,9 +1,9 @@
 {host, ...}: {
   imports = [
-    ../../hosts/${host}/system.nix # import host specific system configuration
     ./decky-loader.nix
     ./device.nix
     ./hardware.nix
     ./steam.nix
+    ../../hosts/${host}/system.nix # import host specific system configuration
   ];
 }
