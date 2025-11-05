9307de071eb895a44c58868750e4f566d1253654
peanutbother <peanutbother@proton.me>
2025-11-05 15:20:30 +0100

    update uuids again

--- hosts/yuserve/hardware.nix
+++ hosts/yuserve/hardware.nix
@@ -13,11 +13,11 @@
   };
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/bf3f2590-e002-4fe0-9632-cba976824ad8";
+    device = "/dev/disk/by-uuid/2d10fb84-44e6-4cf4-8731-7460e6532b93";
     fsType = "ext4";
   };
 
   swapDevices = [
-    {device = "/dev/disk/by-uuid/699bb45c-a0f3-42f2-bec3-43ea532de590";}
+    {device = "/dev/disk/by-uuid/accd7028-a5e1-4b1d-aef7-2f03c6f5972e";}
   ];
 }
