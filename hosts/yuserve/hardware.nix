d25994495a0209ec8050ed07346805d7fec0cfb2
peanutbother <peanutbother@proton.me>
2025-11-05 14:33:59 +0100

    fix disk ids

--- hosts/yuserve/hardware.nix
+++ hosts/yuserve/hardware.nix
@@ -5,11 +5,11 @@
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/69fedf39-dd6e-41be-86cf-97506f79744d";
+    device = "/dev/disk/by-uuid/bf3f2590-e002-4fe0-9632-cba976824ad8";
     fsType = "ext4";
   };
 
   swapDevices = [
-    {device = "/dev/disk/by-uuid/ba5304e2-89fa-4c70-897f-31915dc9780c";}
+    {device = "/dev/disk/by-uuid/699bb45c-a0f3-42f2-bec3-43ea532de590";}
   ];
 }
