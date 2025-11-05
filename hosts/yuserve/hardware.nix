957ed90556d8d11dbf4dc22d04e8a9079f0b6ec4
peanutbother <peanutbother@proton.me>
2025-11-05 17:29:29 +0100

    partuuids are not really stable on xen

--- hosts/yuserve/hardware.nix
+++ hosts/yuserve/hardware.nix
@@ -13,11 +13,11 @@
   };
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/2d10fb84-44e6-4cf4-8731-7460e6532b93";
+    device = "/dev/xvda1";
     fsType = "ext4";
   };
 
   swapDevices = [
-    {device = "/dev/disk/by-uuid/accd7028-a5e1-4b1d-aef7-2f03c6f5972e";}
+    {device = "/dev/xvda2";}
   ];
 }
