b3fa409662505a3bb4c34b874f79cbfea5288857
peanutbother <peanutbother@proton.me>
2026-06-01 15:56:34 +0200

    update istat-menus

--- overlays/istat-menus.nix
+++ overlays/istat-menus.nix
@@ -3,10 +3,10 @@ inputs: final: prev: let
   lib = inputs.nixpkgs.lib;
 in {
   istat-menus = prev.istat-menus.overrideAttrs (_: prev: rec {
-    version = "7.20";
+    version = "7.30";
     src = fetchurl {
       url = "https://cdn.istatmenus.app/files/istatmenus${lib.versions.major version}/versions/iStatMenus${version}.zip";
-      hash = "sha256-oJApYp7ejtcMrm7CyeohV/euXYkJJ0yCRBW2i5AgcEE=";
+      hash = "sha256-qCgMEUjHUsEP+B+e2nylse9T/Xnt765RzV0WtBSWSPY=";
     };
   });
 }
